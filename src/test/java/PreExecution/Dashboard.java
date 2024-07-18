cbba9852931dbecbe65fa0f26d90c1b1ae27b1eb
Testing <100554679+S2605@users.noreply.github.com>
2024-07-18 11:57:51 +0530

    Offer card changes

--- src/test/java/PreExecution/Dashboard.java
+++ src/test/java/PreExecution/Dashboard.java
@@ -44,46 +44,75 @@ public class Dashboard extends LibGlobal {
 		closebtn.click();
 
 		String windowHandle = driver.getWindowHandle();
-		System.out.println(windowHandle + "windowHandle");
+		// System.out.println(windowHandle + "windowHandle");
 		WebElement FDOffercard = wait.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getFD_OfferCard()));
 		FDOffercard.click();
 		Thread.sleep(10000);
 		getWindowHandles(windowHandle);
 
-		WebElement DigitalFDOffercard = wait.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getDigitalFD_OfferCard()));
+		String windowHandle1 = driver.getWindowHandle();
+		WebElement DigitalFDOffercard = wait
+				.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getDigitalFD_OfferCard()));
 		DigitalFDOffercard.click();
+		// System.out.println(DigitalFDOffercard + windowHandle1 + "Check windows
+		// handle");
 		Thread.sleep(10000);
-		getWindowHandles(windowHandle);
+		getWindowHandles(windowHandle1);
 
-//		WebElement FIPOffercard = wait.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getFIP_OfferCard()));
-//		FIPOffercard.click();
-//		WebElement twoWheelerCard = wait.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getTwoWheeler_offerCard()));
-//		twoWheelerCard.click();
-//		WebElement personalLoanCard = wait.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getPersonalLoan_offercard()));
-//		personalLoanCard.click();
-//		WebElement goldLoanCard = wait.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getGoldLoan_Offercard()));
-//		goldLoanCard.click();
-//		WebElement FourWheelerCard = wait.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getFourWheeler_offerCard()));
-//		FourWheelerCard.click();
-//		WebElement businessLoanCard = wait.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getBusinessLoan_offerCard()));
-//		businessLoanCard.click();
+		String windowHandle2 = driver.getWindowHandle();
+		WebElement FIPOffercard = wait
+				.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getFIP_OfferCard()));
+		FIPOffercard.click();
+		Thread.sleep(10000);
+		getWindowHandles(windowHandle2);
+
+		String windowHandle3 = driver.getWindowHandle();
+		WebElement twoWheelerCard = wait
+				.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getTwoWheeler_offerCard()));
+		twoWheelerCard.click();
+		Thread.sleep(10000);
+		getWindowHandles(windowHandle3);
+
+		WebElement rightArrow = wait.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getRightArrow()));
+		rightArrow.click();
+		Thread.sleep(5000);
+
+		String windowHandle4 = driver.getWindowHandle();
+		WebElement personalLoanCard = wait
+				.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getPersonalLoan_offercard()));
+		personalLoanCard.click();
+		Thread.sleep(10000);
+		getWindowHandles(windowHandle4);
+
+		WebElement rightArrow1 = wait.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getRightArrow()));
+		rightArrow1.click();
+		Thread.sleep(5000);
+		String windowHandle5 = driver.getWindowHandle();
+		WebElement goldLoanCard = wait
+				.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getGoldLoan_Offercard()));
+		goldLoanCard.click();
+		getWindowHandles(windowHandle5);
+
+		WebElement rightArrow2 = wait.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getRightArrow()));
+		rightArrow2.click();
+		Thread.sleep(5000);
+
+		String windowHandle6 = driver.getWindowHandle();
+		WebElement FourWheelerCard = wait
+				.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getFourWheeler_offerCard()));
+		FourWheelerCard.click();
+		getWindowHandles(windowHandle6);
+
+		WebElement rightArrow3 = wait.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getRightArrow()));
+		rightArrow3.click();
+		Thread.sleep(5000);
+		String windowHandle7 = driver.getWindowHandle();
+		WebElement businessLoanCard = wait
+				.until(ExpectedConditions.visibilityOf(Xpath_sfl.getInstance().getBusinessLoan_offerCard()));
+		businessLoanCard.click();
+		Thread.sleep(5000);
+		getWindowHandles(windowHandle7);
 
 	}
-
-//		if (Page_URL.contains("https://sitsfl.stfc.in/")) {
-//
-//			System.out.println(Page_URL + "user sucessfully landed");
-//
-//		} else {
-//
-//			System.out.println("user hit the  wrong domain");
-//		}
-
-//		Thread.sleep(2000);
-//		Actions actions = new Actions(driver);
-//		WebElement fd_OfferCard = Xpath_sfl.getInstance().getFD_OfferCard();
-//		actions.moveToElement(fd_OfferCard).perform();
-//		Thread.sleep(1000);
-//		btnClick(Xpath_sfl.getInstance().getFD_OfferCard());
 
 }
